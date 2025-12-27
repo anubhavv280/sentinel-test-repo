42d30c4de225baa4ab1dfc6265eba065128fc474
anubhavv280 <anubhavv280@gmail.com>
2025-12-27 23:32:48 +0530

    Create UserLogger1.java

---
+++ UserLogger1.java
@@ -0,0 +1,10 @@
+import java.util.logging.Logger;
+
+public class UserLogger1 {
+    private static final Logger log = Logger.getLogger(UserLogger.class.getName());
+
+    public void logUserData(String email, String phoneNumber) {
+        // Check 3: Sensitive data in logs (LOGGING AUDIT ISSUE)
+        log.info("Processing user with email: " + email + " and phone: " + phoneNumber);
+    }
+}
